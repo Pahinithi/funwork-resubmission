b5d12c7b9996df3b56c017d04939d3b4636d3eaf
Pahinithi <nithilan32@gmail.com>
2020-02-19 13:59:57 +0530

    funwork1 resubmision

---
+++ main.js
@@ -0,0 +1,496 @@
+SC.initialize({
+  client_id: '340f063c670272fac27cfa67bffcafc4'
+});
+
+$(document).ready(function() {
+//verithanam
+SC.stream('/tracks/717303730',function(sound){
+ -+
+$('#start0').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+$('#stop0').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+});
+
+
+//munbe-vaa
+SC.stream('/tracks/345946410',function(sound){
+
+$('#start1').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+$('#stop1').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+});
+
+
+//Usure Pogudhey
+SC.stream('/tracks/345819962',function(sound){
+
+$('#start2').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+$('#stop2').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+});
+
+
+//Kadal rojava
+SC.stream('/tracks/465853140',function(sound){
+
+$('#start3').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+$('#stop3').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+});
+
+
+//KANAVE KANAVE
+SC.stream('/tracks/73473040',function(sound){
+
+$('#start4').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+$('#stop4').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+});
+
+
+
+//Devathi thadi
+SC.stream('/tracks/245201802',function(sound){
+
+$('#start5').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+$('#stop5').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+});
+
+
+//Azagi
+SC.stream('/tracks/245201802',function(sound){
+
+$('#start6').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+$('#stop6').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+});
+
+
+
+//Mecchuko Mrchhukoo
+SC.stream('/tracks/327516254',function(sound){
+
+$('#start7').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+$('#stop7').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+});
+
+
+//Seeti Maar
+SC.stream('/tracks/328175520',function(sound){
+
+$('#start8').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+$('#stop8').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+});
+
+
+//Arere YekkadA
+SC.stream('/tracks/303243440',function(sound){
+
+$('#start9').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+$('#stop9').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+});
+
+
+//Next Enti
+SC.stream('/tracks/302365070',function(sound){
+
+$('#start10').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+$('#stop10').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+});
+
+
+//Ee Premaki
+SC.stream('/tracks/488960430',function(sound){
+
+$('#start11').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+$('#stop11').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+});
+
+
+//Vaarthinkalee | kali malayalam movie
+SC.stream('/tracks/251370150',function(sound){
+
+$('#start12').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+$('#stop12').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+});
+
+
+
+//Vachinda
+SC.stream('/tracks/344830827',function(sound){
+
+$('#start13').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+$('#stop13').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+});
+
+
+//Kala-Kala-Kalamandir
+SC.stream('/tracks/573166107',function(sound){
+
+$('#start14').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+$('#stop14').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+});
+
+
+
+//Malare Ninne Kanathirunnal - Premam
+SC.stream('/tracks/209020178',function(sound){
+
+$('#start15').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+$('#stop15').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+});
+
+
+//Chinna Chinna - Premam
+SC.stream('/tracks/209395244',function(sound){
+
+$('#start16').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+$('#stop16').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+});
+
+
+//Rockaankuthu
+SC.stream('/tracks/202301616',function(sound){
+
+$('#start17').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+$('#stop17').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+});
+
+
+//kali
+SC.stream('/tracks/251364934',function(sound){
+
+$('#start18').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+$('#stop18').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+});
+
+
+//Vaanam Thilathilakkanu - CIA
+SC.stream('/tracks/251364934',function(sound){
+
+$('#start19').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+$('#stop19').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+});
+
+
+//Solo
+SC.stream('/tracks/807491622',function(sound){
+
+$('#start20').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+$('#stop20').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+});
+
+
+//Pavizha Mazhaye
+SC.stream('/tracks/612105816',function(sound){
+
+$('#start21').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+$('#stop21').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+});
+
+
+//Dhoom Again
+SC.stream('/tracks/200458850',function(sound){
+
+$('#start22').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+$('#stop22').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+});
+
+
+//War
+SC.stream('/tracks/684897301',function(sound){
+
+$('#start23').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+$('#stop23').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+});
+
+
+//War1
+SC.stream('/tracks/676605290',function(sound){
+
+$('#start24').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+$('#stop24').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+});
+
+
+
+//Raavan
+SC.stream('/tracks/157057662',function(sound){
+
+$('#start25').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+$('#stop25').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+});
+
+
+
+
+
+//Mass Leader
+SC.stream('/tracks/338763253',function(sound){
+
+$('#start26').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+$('#stop26').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+});
+
+
+
+
+//Joker
+SC.stream('/tracks/131141499',function(sound){
+
+$('#start27').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+$('#stop27').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+});
+
+
+
+//John
+SC.stream('/tracks/635345019',function(sound){
+
+$('#start28').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+$('#stop28').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+});
+
+
+//Singa penna
+SC.stream('/tracks/652012811',function(sound){
+
+$('#start29').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+$('#stop29').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+});
+
+//
+//Singa penna
+SC.stream('/tracks/717303730',function(sound){
+
+$('#start30').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+$('#stop30').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+});
+
+
+SC.stream('/tracks/53046942',function(sound){
+
+$('#start31').click(function(e) {
+           e.preventDefault();
+           sound.start();
+         });
+
+$('#stop31').click(function(e) {
+           e.preventDefault();
+           sound.stop();
+         });
+
+ });
+
+});
